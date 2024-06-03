c4690fe40a8e9d9f1741d5c8b926ea5646013dda
Sonny Cooper <ed22699@bristol.ac.uk>
2024-06-03 13:49:36 +0100

    Create the FolderStuctureDisplay window

---
+++ app/Views/FolderStuctureDisplay.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace app.Views;
+
+public partial class FolderStuctureDisplay : UserControl
+{
+    public FolderStuctureDisplay()
+    {
+        InitializeComponent();
+    }
+}
