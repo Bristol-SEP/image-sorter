a3f2910abd0a293780595d566004ddb6419650c6
Sonny Cooper <ed22699@bristol.ac.uk>
2024-06-04 11:16:35 +0100

    Basic structure

---
+++ Image-Sorter-UI/app.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Image_Sorter_UI;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
