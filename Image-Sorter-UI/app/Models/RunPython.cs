ea765c971a495484ca882e72271c6cb12b1ce8c4
Jackwayt <bo22354@bristol.ac.uk>
2024-07-25 12:12:00 +0100

    Should now be able to use python script runner from linux and mac

--- Image-Sorter-UI/app/Models/RunPython.cs
+++ Image-Sorter-UI/app/Models/RunPython.cs
@@ -1,30 +1,45 @@
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Python.Runtime;
 
 namespace app.Models;
 
 public class RunPython
 {
-   // TODO set function/class to take in a parameter script and run it with variables (features)
-   // test script can be found in app.Test/Assets/testPython
+   // Runs a python enviroment so that it can then take in some parameters and a python script and run it
    public bool RunScript()
    {
       // TODO this is where the issue is coming from, cannot find correct DLL and how to package it 
       // so can run on others computers
       // see app.Test/Models/RunPythonTest this is a useful way of running this script in isolation
-      // Runtime.PythonDLL = @"C:\Python312\python312.dll";
-      // Console.WriteLine("here start");
 
-      // Ensure the Python DLL path is correct
-      string pythonDllPath = @"C:\Python312\python312.dll";
-      if (!File.Exists(pythonDllPath)){
-         Console.WriteLine($"Python DLL not found at {pythonDllPath}");
+
+      //Determine the OS to find and set the correct path for the pythonDLL
+      string pythonDllPath;
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)){
+         pythonDllPath = @"C:\Python312\python312.dll";
+      }else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)){
+         pythonDllPath = @"/usr/local/opt/python@3.12/Frameworks/Python.framework/Versions/3.12/lib/libpython3.12.dylib";
+      }else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)){
+         pythonDllPath = @"/usr/lib/x86_64-linux-gnu/libpython3.12.so";
+      }else{
+         Console.WriteLine("Unsupported File directory");
          return false;
       }
 
+      if (!File.Exists(pythonDllPath)){
+         //At a later date would want to make a file selector for if in a different location
+         //Can't really do this yet though as need an interface to work with rather than just tests
+         Console.WriteLine($"Python DLL not found at {pythonDllPath}"); 
+         return false;
+      }
       Runtime.PythonDLL = pythonDllPath;
       Console.WriteLine("Python DLL path set");
+
+
+
 
       // Set the Python script path
       //Currently the working directory is going from the tests ("Coding Projects\image-sorter\Image-Sorter-UI\app.Test\bin\Debug\net6.0\")
@@ -40,13 +55,10 @@ public class RunPython
       string pythonScriptPath = Path.Combine(baseDirectory, relativePythonScriptPath);
       Console.WriteLine($"Python Script Directory {pythonScriptPath}");
 
-
       if (!Directory.Exists(pythonScriptPath)){
          Console.WriteLine($"Python script directory not found at {pythonScriptPath}");
          return false;
       }
-
-
 
       try{
          // Initialize engine
@@ -54,10 +66,8 @@ public class RunPython
          Console.WriteLine("Python engine initialized");
          
          // Create a scope for the Python code
-         using (Py.GIL())
-         {
-            try
-            {
+         using (Py.GIL()){
+            try{
                PythonEngine.Exec($"import sys; sys.path.append(r'{pythonScriptPath}')");
                Console.WriteLine($"Python script path appended: {pythonScriptPath}");
 
@@ -65,26 +75,24 @@ public class RunPython
                dynamic pyScript = Py.Import("testPython");
                Console.WriteLine("Python script imported");
 
-               // call function from python script
+               // Call function from python script
                dynamic result = pyScript.noParamTest();
                Console.WriteLine($"Result from Python script: {result}");
+               if(result == false){return false;}
 
                result = pyScript.paramTest(1, 2);
                Console.WriteLine($"Result from Python script: {result}");
+               if(result == false){return false;}
 
                result = pyScript.manyParamTest(1, 2, 3, 4);
                Console.WriteLine($"Result from Python script: {result}");
-
-
-
+               if(result == false){return false;}
             }
             catch (Exception e)
             {
                Console.WriteLine($"An error occurred after initalisation: {e.Message}");
                return false;
-
             }
-
       }
       }catch (Exception e){
          Console.WriteLine($"An error occurred before initialisation: {e.Message}");
