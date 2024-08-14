f5d16b122a26495fffcb89a6e59dd38743dc14b8
Sonny Cooper <ed22699@bristol.ac.uk>
2024-08-14 22:48:44 +0100

    Implement basic script to copy all images
    Copies all images and moves them to a folder dependent on its metadata
    description

--- Image-Sorter-UI/app/Scripts/BoatNumberScript.sh
+++ Image-Sorter-UI/app/Scripts/BoatNumberScript.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
-echo "entered"
-echo $1
-echo $2
+
+# Create the variables
+affectedFolder=$1
+targetFolder=$1
+
+# Enter the folder
+cd "$affectedFolder" || exit
+
+# Search for files and read metadata
+allImages=$(find . -type f -name "*.jpg")
+for image in $allImages; do
+    description=$(exiftool -s3 -Description "$image")
+    # if folder not already present create it
+    if [ ! -d "$description" ]; then
+        mkdir "$description"
+    fi
+    cp "$image" "$description/"
+done
+
