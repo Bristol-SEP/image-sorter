20ea60e67ecd81fea36d95ea83950fae28349c66
Sonny Cooper <ed22699@bristol.ac.uk>
2024-08-17 08:40:20 +0100

    Implement basic working script

--- Image-Sorter-UI/app/Scripts/BoatNumberScript.sh
+++ Image-Sorter-UI/app/Scripts/BoatNumberScript.sh
@@ -5,18 +5,26 @@
 affectedFolder=$1
 targetFolder=$1
 
-# Enter the folder
-cd "$affectedFolder" || exit
+# Enter the folder to search through
+cd "$targetFolder" || exit
 
 # Search for files and read metadata
 allImages=$(find . -type f -name "*.jpg")
+
+# Enter the folder to write to
+cd "$affectedFolder" || exit
+
+if [ ! -d "Boat Clubs" ]; then
+    mkdir "Boat Clubs"
+fi
+
 for image in $allImages; do
     description=$(exiftool -s3 -Description "$image")
     boatNum=$(awk -F'Boat Code:' '{print $2}' <<< "$description" | cut -d "." -f1)
     # if folder not already present create it
-    if [ ! -d "$boatNum" ]; then
-        mkdir "$boatNum"
+    if [ ! -d "Boat Clubs/$boatNum" ]; then
+        mkdir "Boat Clubs/$boatNum"
     fi
-    cp "$image" "$boatNum/"
+    cp "$image" "Boat Clubs/$boatNum/"
 done
 
