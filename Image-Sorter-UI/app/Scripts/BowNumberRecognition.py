e90a56416bddd02c2d7fb39fee4c749f17dff94e
Sonny Cooper <ed22699@bristol.ac.uk>
2024-09-01 09:20:36 +0100

    Hard coded cropping, reading test

--- Image-Sorter-UI/app/Scripts/BowNumberRecognition.py
+++ Image-Sorter-UI/app/Scripts/BowNumberRecognition.py
@@ -2,8 +2,8 @@ import cv2
 from matplotlib import pyplot as plt
 import imutils
 import numpy as np
-import re
 from PIL import Image
+import pytesseract
 
 
 def detectEdge(image):
@@ -15,47 +15,22 @@ def detectEdge(image):
     # noise reduction
     bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
 
-    # edge detection
-    edged = cv2.Canny(bfilter, 30, 30)
+    # Threshold
+    ret, th = cv2.threshold(bfilter, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    # contour detection
-    keypoints = cv2.findContours(edged.copy(),
-                                 cv2.RETR_TREE,
-                                 cv2.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(keypoints)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+    crop_image = img[2300:2440, 440:560]
+    boat_code = img[2500:2580, 1780:2050]
 
-    # finding box
-    location = None
-    for contour in contours:
-        approx = cv2.approxPolyDP(contour, 10, True)
-        if len(approx) == 4:
-            location = approx
-            break
+    # plt.imshow(boat_code)
+    # plt.show()
+    readText(crop_image)
+    readText(boat_code)
 
-    # blocking out everything exept number plate
-    mask = np.zeros(gray.shape, np.uint8)
-    new_image = cv2.drawContours(mask, [location], 0, 225, -1)
-    new_image = cv2.bitwise_and(img, img, mask=mask)
 
-    # turns screenCnt into seperate coordinates
-    num1 = [int(s) for s in re.findall(r'\b\d+\b', str(location[0]))]
-    num2 = [int(s) for s in re.findall(r'\b\d+\b', str(location[1]))]
-    num3 = [int(s) for s in re.findall(r'\b\d+\b', str(location[2]))]
-    num4 = [int(s) for s in re.findall(r'\b\d+\b', str(location[3]))]
-
-    x1, x2, x3, x4 = num1[0], num2[0], num3[0], num4[0]
-    y1, y2, y3, y4 = num1[1], num2[1], num3[1], num4[1]
-
-    top_x = min([x1, x2, x3, x4])
-    left_y = min([y1, y2, y3, y4])
-    bot_x = max([x1, x2, x3, x4])
-    right_y = max([y1, y2, y3, y4])
-
-    cropped_image = gray[left_y:right_y+1, top_x:bot_x+1]
-
-    imgplot = plt.imshow(img)
-    plt.show()
+def readText(img):
+    # reads number plate
+    text = pytesseract.image_to_string(img, config='--psm 11')
+    print(text)
 
 
 detectEdge("testImage.jpg")
