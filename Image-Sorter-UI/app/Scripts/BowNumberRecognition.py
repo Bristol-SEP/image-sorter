668e75b7b29de7f9ab551dcf3e15581495d11abb
Sonny Cooper <ed22699@bristol.ac.uk>
2024-09-05 19:41:29 +0100

    Try some more filters, still picking up boy

--- Image-Sorter-UI/app/Scripts/BowNumberRecognition.py
+++ Image-Sorter-UI/app/Scripts/BowNumberRecognition.py
@@ -1,36 +1,67 @@
 import cv2
-from matplotlib import pyplot as plt
-import imutils
 import numpy as np
-from PIL import Image
-import pytesseract
+import matplotlib.pyplot as plt
+import imutils
+import re
 
+# have a minimum and maximum aspect ratio to help search for the rectangle
 
-def detectEdge(image):
-    # load image
-    img = cv2.imread(image)
-    # gray scaling
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Read input image
+img = cv2.imread("testImage.jpg")
 
-    # noise reduction
-    bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+# convert input image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Threshold
-    ret, th = cv2.threshold(bfilter, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# blackhat morphological transformation
+rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 13))
+blackhat = cv2.morphologyEx(gray, cv2.MORPH_BLACKHAT, rectKern)
 
-    crop_image = img[2300:2440, 440:560]
-    boat_code = img[2500:2580, 1780:2050]
+# next, find regions in the image that are light
+squareKern = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+light = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, squareKern)
+light = cv2.threshold(light, 225, 255,
+                      cv2.THRESH_BINARY)[1]
 
-    # plt.imshow(boat_code)
-    # plt.show()
-    readText(crop_image)
-    readText(boat_code)
+#edge detection
+bfilter = cv2.bilateralFilter(light, 11, 17, 17) #noise reduction
+edged = cv2.Canny(bfilter, 30, 30) #edge detection
 
+# compute the Scharr gradient representation of the blackhat
+# image in the x-direction and then scale the result back to
+# the range [0, 255]
+gradX = cv2.Sobel(blackhat, ddepth=cv2.CV_32F,
+                  dx=1, dy=0, ksize=-1)
+gradX = np.absolute(gradX)
+(minVal, maxVal) = (np.min(gradX), np.max(gradX))
+gradX = 255 * ((gradX - minVal) / (maxVal - minVal))
+gradX = gradX.astype("uint8")
 
-def readText(img):
-    # reads number plate
-    text = pytesseract.image_to_string(img, config='--psm 11')
-    print(text)
+# find contours in the thresholded image and sort them by
+# their size in descending order, keeping only the largest
+# ones
+keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contours = imutils.grab_contours(keypoints)
+contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
+# initialize the license plate contour and ROI
+lpCnt = None
+roi = None
+# loop over the license plate candidate contours
+for c in contours:
+    # compute the bounding box of the contour and then use
+    # the bounding box to derive the aspect ratio
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w / float(h)
 
-detectEdge("testImage.jpg")
+    # check to see if the aspect ratio is rectangular
+    if ar >= 1 and ar <= 2:
+        # store the license plate contour and extract the
+        # license plate from the grayscale image and then
+        # threshold it
+        lpCnt = c
+        licensePlate = gray[y:y + h, x:x + w]
+        roi = cv2.threshold(licensePlate, 0, 255,
+                            cv2.THRESH_BINARY_INV)[1]
+
+plt.imshow(light)
+plt.show()
